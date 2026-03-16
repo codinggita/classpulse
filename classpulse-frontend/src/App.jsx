22942b31923c1da075aeef4f73c044890d7fde70
Ammar-Meman <ammar.meman.cg@gmail.com>
2026-03-16 05:59:52 +0530

    Restore VotePage.jsx and update App.jsx

--- classpulse-frontend/src/App.jsx
+++ classpulse-frontend/src/App.jsx
@@ -1,12 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import JoinSession from "./components/JoinSession";
-// import VotePage from "./components/VotePage";
+import VotePage from "./components/VotePage";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<JoinSession />} />
-      {/* <Route path="/vote" element={<VotePage />} /> */}
+      <Route path="/vote" element={<VotePage />} />
     </Routes>
   );
 }
